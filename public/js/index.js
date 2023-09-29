f7c337c66d5c8af01fe7f32c9bd72106d0eb4ec5
OAN <oan@fedora.home>
2023-09-29 19:31:21 +0200

    fix: matching between the number array and the word log

--- public/js/index.js
+++ public/js/index.js
@@ -14,7 +14,7 @@
     function transform(params) {
         const arg = [...params];
         for ( let i = 0 ; i < arg.length ; i++ ) {
-			if (arg[i] % 3 === 0 && i % 5 === 0) {
+			if (arg[i] % 15 === 0 ) {
 				console.log('fizzbuzz', arg[i]);
 			}
             else if(arg[i] % 3 === 0) {
@@ -27,20 +27,13 @@
                 console.log('nothing', arg[i]);
             }
     }
-	// for (let i = 0; i < arg.length; i++) {
-	// 	if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz",arg[i]);
-	// 	else if (i % 3 == 0) console.log("Fizz",arg[i]);
-	// 	else if (i % 5 == 0) console.log("Buzz",arg[i]);
-	// 	else console.log('Nothing',arg[i]);
-	// }
-
 }
 console.log('transform argument tab 1 : ');
 transform([3,5]);
 console.log('transform argument tab 2 : ');
 transform([0,15]);
 console.log('transform argument tab 3 : ');
-transform([1, 2, 3, 4, 5]);
+transform([1, 2, 4, 5]);
 console.log('transform argument tab 4 : ');
 transform([115, 90, 99, 100, 5421, 667]);
 console.log('transform argument tab 5 : ');
