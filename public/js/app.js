82d349acd3de714429c18b41bddebf6cd2110bcb
OAN <oan@fedora.home>
2023-09-30 14:07:53 +0200

    feat : comments for explaining

--- public/js/app.js
+++ public/js/app.js
@@ -5,29 +5,30 @@
      * 
      * for each array element
      *  condition : 
-     *       if ( === 3 ) {fizz}
-     *       else if ( === 5 ) {buzz}
+			 if (arg[i] % 3 === 0 && arg[i] % 5=== 0) {fizzbuzz}
+     *       else if ( arg[i] === 3 ) {fizz}
+     *       else if (arg[i]=== 5 ) {buzz}
      *       else { nothing}
      */
 
 	
     function transform(params) {
-        const arg = [...params];
+        const arg = [...params]; //the local varible fot the for looping 
         for ( let i = 0 ; i < arg.length ; i++ ) {
-			if (arg[i] % 15 === 0 ) {
-				console.log('fizzbuzz', arg[i]);
-			}
-            else if(arg[i] % 3 === 0) {
-				console.log('fizz', arg[i]);
-            }
-            else if (arg[i]% 5 === 0){
-				console.log('buzz', arg[i]);
-            }
-            else {
-                console.log('nothing', arg[i]);
-            }
-    }
-}
+				if (arg[i] % 15 === 0 ) {//  the 3 && 5	
+					console.log('fizzbuzz');
+				}
+				else if(arg[i] % 3 === 0) { // only 3
+					console.log('fizz');
+				}
+				else if (arg[i]% 5 === 0){ // only 5
+					console.log('buzz');
+				}
+				else { // all other cases
+					console.log('nothing');
+				}
+    	}
+	}
 console.log('transform argument tab 1 : ');
 transform([3,5]);
 console.log('transform argument tab 2 : ');
